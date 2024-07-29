cecb7abce8dcbdd0cb080a6b1bbd6ebb8797033d
Ahmed Gaber <agaber@futureface.sa>
2024-07-29 16:53:48 +0300

    create header commponent

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+    <section class="header">
+        <div class="container">
+            <div class="">
+                
+            </div>
+        </div>
+    </section>
+</template>
+<style lang="scss" scoped>
+
+</style>
